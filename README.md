db640b3c316f6f81de49fe4e6c26f2869ddcb0b4
William.Weng <linuxice0609@gmail.com>
2021-12-21 15:51:44 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WWCountdownButton
+
+A description of this package.
---
+++ Tests/WWCountdownButtonTests/WWCountdownButtonTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WWCountdownButton
+
+final class WWCountdownButtonTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WWCountdownButton().text, "Hello, World!")
+    }
+}
