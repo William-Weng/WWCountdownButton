01851601532b1693df8fb99b007857ad8c4a94a4
William.Weng <William.Weng@iit.com.hk>
2023-09-20 13:40:20 +0800

    - 增加暫停 / 歸零的功能

--- Sources/WWCountdownButton/CountdownButton.swift
+++ Sources/WWCountdownButton/CountdownButton.swift
@@ -13,21 +13,35 @@ open class WWCountdownButton: UIButton {
     
     public typealias CountdownInformation = (times: [Int], isFinish: Bool)
     
+    public var isPaused: Bool = false {
+        willSet { pauseControl(isPaused: newValue, isCountdown: self.isCountdown) }
+    }
+    
     private var isCountdown = true
-    private var second: TimeInterval = 0
+    private var countdownSecond: TimeInterval = 0
     private var startDate = Date()
+    private var pauseDate = Date()
     private var timer: CADisplayLink?
     private var resultClourse: ((CountdownInformation) -> Void)?
+    private var elapsedSecond: TimeInterval = 0
+    
+    @objc private func startCountdown(_ sender: CADisplayLink) { countdownAction(from: startDate, isCountdown: isCountdown) }
+}
+
+// MARK: 公開工具
+public extension WWCountdownButton {
     
     /// 開始倒數 => 建議使用『等寬字型』
     /// - Parameters:
     ///   - second: UInt
     ///   - isCountdown: Bool
     ///   - reuslt: ((CountdownInformation) -> Void)
-    public func countdown(second: UInt = 10, isCountdown: Bool = true, reuslt: ((CountdownInformation) -> Void)? = nil) {
+    func countdown(second: UInt = 5, isCountdown: Bool = true, reuslt: ((CountdownInformation) -> Void)? = nil) {
+        
+        timerClean()
         
         self.startDate = Date()
-        self.second = TimeInterval(second)
+        self.countdownSecond = TimeInterval(second)
         self.resultClourse = reuslt
         self.isCountdown = isCountdown
         
@@ -36,47 +50,86 @@ open class WWCountdownButton: UIButton {
         timer?._fire()
     }
     
-    @objc private func startCountdown(_ sender: CADisplayLink) { countdownAction(from: startDate, isCountdown: isCountdown) }
+    /// 回歸初始值 (歸零)
+    func reset() {
+        
+        let times = (!isCountdown) ? dateComponents(with: 0) : dateComponents(with: self.countdownSecond)
+        
+        titleSetting(with: times)
+        timerClean()
+    }
 }
 
 // MARK: 小工具
-extension WWCountdownButton {
+private extension WWCountdownButton {
     
     /// 倒數的功能
     /// - Parameters:
     ///   - startDate: Date
     ///   - isCountdown: Bool
-    private func countdownAction(from startDate: Date, isCountdown: Bool = true) {
+    func countdownAction(from startDate: Date, isCountdown: Bool = true) {
         
-        guard let components = countdownNumbers(from: startDate, isCountdown: isCountdown),
-              let minute = components.times.first,
-              let second = components.times.last
+        guard let components = countdownNumbers(from: startDate, isCountdown: isCountdown) else { return }
+        
+        if (components.isFinish) { timerClean() }
+        
+        titleSetting(with: components.times)
+        resultClourse?(components)
+    }
+    
+    /// 設定Button標題 (00:00)
+    /// - Parameter times: [Int]
+    func titleSetting(with times: [Int]) {
+        
+        guard let minute = times.first,
+              let second = times.last
         else {
             return
         }
         
-        if (components.isFinish) { timerClean() }
-        
         setTitle("\(String(format: "%02d", minute)):\(String(format: "%02d", second))", for: .normal)
-        resultClourse?(components)
     }
     
-    /// 倒數的相關資訊
+    /// 倒數的相關資訊 (與現在的時間相減)
     /// - Parameters:
     ///   - startDate: Date
     ///   - isCountdown: Bool
     /// - Returns: CountdownInformation
-    private func countdownNumbers(from startDate: Date, isCountdown: Bool = true) -> CountdownInformation? {
+    func countdownNumbers(from startDate: Date, isCountdown: Bool = true) -> CountdownInformation? {
         
-        let second = Date() - startDate
-        let countdownSecond = self.second - second + 1
-        let components = Date(timeIntervalSince1970: (isCountdown) ? countdownSecond : second)._components([.minute, .second])
+        let elapsedSecond = Date() - startDate
+        let countdownSecond = countdownSecond - elapsedSecond + 1
+        var components = dateComponents(with: elapsedSecond)
         
-        return (components, Int(countdownSecond) == 0)
+        if (isCountdown) { components = dateComponents(with: countdownSecond) }
+        self.elapsedSecond = elapsedSecond
+        
+        return (components, Int(countdownSecond) <= 0)
+    }
+    
+    /// 秒數時間轉換 (90秒 => 1分30秒)
+    /// - Parameter second: TimeInterval
+    /// - Returns: [Int]
+    func dateComponents(with second: TimeInterval) -> [Int] {
+        
+        let components = Date(timeIntervalSince1970: second)._components([.minute, .second])
+        return components
+    }
+    
+    /// 倒數暫停
+    /// - Parameters:
+    ///   - isPaused: 是否要暫停？
+    ///   - isCountdown: 是否是倒數？
+    func pauseControl(isPaused: Bool, isCountdown: Bool) {
+                
+        let elapsedSecond = -(self.elapsedSecond + 1)
+        
+        timer?.isPaused = isPaused
+        startDate = Date().addingTimeInterval(elapsedSecond)
     }
     
     /// Timer歸零
-    private func timerClean() {
+    func timerClean() {
         timer?.invalidate()
         timer = nil
     }
